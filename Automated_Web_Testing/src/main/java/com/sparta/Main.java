95fce738983bdadc560c96d1e6305bfe5ae8be44
Tomasz Wolak <tom.wolak@outlook.com>
2024-05-31 15:28:33 +0100

    Dev branch setup

--- Automated_Web_Testing/src/main/java/com/sparta/Main.java
+++ Automated_Web_Testing/src/main/java/com/sparta/Main.java
@@ -2,6 +2,7 @@ package com.sparta;
 
 public class Main {
     public static void main(String[] args){
+        // Automation Web Testing Project
         System.out.println("Hello, Nish and Phil");
     }
 }
