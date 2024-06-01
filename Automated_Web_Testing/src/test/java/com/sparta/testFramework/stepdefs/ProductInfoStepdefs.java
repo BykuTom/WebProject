49db8e39c529247695c69ac3532db937c141ec2c
Duncan <mspam3453@gmail.com>
2024-06-01 23:49:34 +0100

    removed unnecessary broken code

--- Automated_Web_Testing/src/test/java/com/sparta/testFramework/stepdefs/ProductInfoStepdefs.java
+++ Automated_Web_Testing/src/test/java/com/sparta/testFramework/stepdefs/ProductInfoStepdefs.java
@@ -22,43 +22,7 @@ import java.time.Duration;
 
 import static org.junit.Assert.assertTrue;
 
-public class ProductInfoStepdefs {
-    private static ChromeDriverService service;
-    private static final String DRIVER_LOCATION = "src/test/resources/chromedriver.exe";
-
-    WebDriver webDriver;
-
-    public ChromeOptions getChromeOptions(){
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        options.addArguments("--headless");
-        options.addArguments("--remote-allow-origins=*");
-        return options;
-    }
-
-    @BeforeAll
-    public static void beforeAll() throws IOException {
-        service = new ChromeDriverService.Builder()
-                .usingDriverExecutable(new File(DRIVER_LOCATION))
-                .usingAnyFreePort()
-                .build();
-        service.start();
-    }
-
-    @Before
-    public void setUp(){
-        webDriver = new RemoteWebDriver(service.getUrl(), getChromeOptions());
-    }
-
-    @After
-    public void afterEach(){
-        webDriver.quit();
-    }
-
-    @AfterAll
-    public static void afterAll(){
-        service.stop();
-    }
+public class ProductInfoStepdefs extends abstractStepdef {
 
     @Given("the customer is on the homepage")
     public void theCustomerIsOnTheHomepage() {
