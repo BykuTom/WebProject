d1b79c56c81ad77f743500fa72de725f9473caec
christian harborow <christianharborow@hotmail.co.uk>
2024-06-03 22:23:04 +0100

    adds new methods and refactors existing methods

--- Automated_Web_Testing/src/test/java/com/sparta/testFramework/lib/pages/CartPage.java
+++ Automated_Web_Testing/src/test/java/com/sparta/testFramework/lib/pages/CartPage.java
@@ -1,13 +1,22 @@
 package com.sparta.testFramework.lib.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.regex.Pattern;
 
 public class CartPage extends WebPage {
-    private final By titles = By.cssSelector(".item-info .product-item-name");
-    private final By productInfo = By.className("item-info");
-    private final By productTitle = By.className("product-item-name");
+    private static final By PRODUCTS = By.cssSelector(".cart.item");
+    private static final By TITLE = By.className("product-item-name");
+    private static final By QUANTITY = By.cssSelector(".input-text.qty");
+    private static final By COUNTER = By.className("counter-number");
+
+    private WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
 
     public CartPage(WebDriver webDriver) {
         super(webDriver);
@@ -19,18 +28,24 @@ public class CartPage extends WebPage {
         }
     }
 
-    public boolean productIsPresent(String productName) {
-        var titles = webDriver.findElements(this.titles);
-        for (WebElement title : titles) {
-            if (title.getText().equals(productName)) return true;
+    private WebElement getProductEntry(String productName) {
+        for (WebElement product : webDriver.findElements(PRODUCTS)) {
+            String name = product.findElement(TITLE).getText();
+            if (name.equals(productName)) {
+                return product;
+            }
         }
-        return false;
+        return null;
+    }
+
+    public boolean productIsPresent(String productName) {
+        return getProductEntry(productName) != null;
     }
 
     public boolean variantIsPresent(String productName, String size, String colour) {
-        var products = webDriver.findElements(productInfo);
+        var products = webDriver.findElements(PRODUCTS);
         for (WebElement product : products) {
-            String name = product.findElement(productTitle).getText();
+            String name = product.findElement(TITLE).getText();
             if (name.equals(productName)) {
                 var variantAttributes = product.findElements(By.tagName("dd")).stream().map(WebElement::getText).toList();
                 if (variantAttributes.get(0).equals(size) && variantAttributes.get(1).equals(colour)) {
@@ -42,14 +57,34 @@ public class CartPage extends WebPage {
     }
 
     public Integer getProductQty(String productName) {
-        var products = webDriver.findElements(productInfo);
-        for (WebElement product : products) {
-            String name = product.findElement(productTitle).getText();
-            if (name.equals(productName)) {
-                String qty = product.findElement(By.cssSelector(".input-text.qty")).getAttribute("value");
-                return Integer.parseInt(qty);
-            }
-        }
-        return null;
+        WebElement product = getProductEntry(productName);
+        String qty = product.findElement(QUANTITY).getAttribute("value");
+        return Integer.parseInt(qty);
+    }
+
+    public void setProductQty(String productName, int qty) {
+        WebElement product = getProductEntry(productName);
+        product.findElement(QUANTITY).sendKeys(Keys.DELETE, "" + qty);
+    }
+
+    public void clickUpdate() {
+        WebElement update = webDriver.findElement(By.className("update"));
+        update.click();
+        wait.until(ExpectedConditions.stalenessOf(update));
+    }
+
+    public void deleteProduct(String productName) {
+        WebElement product = getProductEntry(productName);
+        product.findElement(By.className("action-delete")).click();
+    }
+
+    public String getEmptyCartMsg() {
+        return webDriver.findElement(By.className("cart-empty")).getText();
+    }
+
+    public int getCartCounter() {
+        wait.until(ExpectedConditions.textMatches(COUNTER, Pattern.compile("\\d+")));
+        String counterText = webDriver.findElement(COUNTER).getText();
+        return Integer.parseInt(counterText);
     }
 }
