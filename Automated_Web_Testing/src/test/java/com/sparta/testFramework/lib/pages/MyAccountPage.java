8e4594581b3120847355e0de8a8829f206c47e63
lachlan-caldwell <33190692+lachlan-caldwell@users.noreply.github.com>
2024-06-04 10:41:04 +0100

    Updating Address Test, Passing

---
+++ Automated_Web_Testing/src/test/java/com/sparta/testFramework/lib/pages/MyAccountPage.java
@@ -0,0 +1,11 @@
+package com.sparta.testFramework.lib.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class MyAccountPage extends WebPage{
+    public MyAccountPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+
+}
